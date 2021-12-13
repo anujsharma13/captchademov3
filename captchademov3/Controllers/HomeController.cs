e842ff6763b846a865d53e4ca36a76a045267fad
anuj sharma <2000anujsharma@gmail.com>
2021-12-13 11:02:03 +0530

    Add project files.

---
+++ captchademov3/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using captchademov3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace captchademov3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Student student = new Student();
+            return View(student);
+        }
+        [HttpPost]
+        public IActionResult Result(Student s)
+        {
+            //var _googlerecaptcha=GoogleCaptchaService.verifyrecaptcha()
+            GoogleCaptchaService googleCaptchaService = new GoogleCaptchaService();
+            var _googlecaptcha = googleCaptchaService.verifycaptcha(s.token);
+            if(!_googlecaptcha.Result.success && _googlecaptcha.Result.score<=0.5)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+            return View("Result", s);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ captchademov3/Models/GoogleCaptchaService.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace captchademov3.Models
+{
+    public class GoogleCaptchaService
+    {
+        public virtual async Task<GoogleCaptchaRespo> verifycaptcha(string _Token)
+        {
+            GoogleCaptchaData data = new GoogleCaptchaData
+            {
+                response = _Token,
+                secret = "6LezVY0dAAAAAHXWLv68LypHJd6NYloRbcgNBLeQ"
+            };
+            HttpClient client = new HttpClient();
+            var response = await client.GetStringAsync($"https://www.google.com/recaptcha/api/siteverify?=secret{data.secret}&response={data.response}");
+            var capresp = JsonConvert.DeserializeObject<GoogleCaptchaRespo>(response);
+            return capresp;
+        }
+    }
+
+    public class GoogleCaptchaData
+    {
+        public string response { get; set; }
+        public string secret { get; set; }
+    }
+    public class GoogleCaptchaRespo
+    {
+        public bool success { get; set; }
+        public double score { get; set; }
+        public string action { get; set; }
+        public DateTime challenge_ts { get; set; }
+        public string hostname { get; set; }
+    }
+}
